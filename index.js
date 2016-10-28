ee0a6900607e7fd030c291362aa6b9be69131b07
satub <satu.barnhill@gmail.com>
2016-10-28 13:35:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much.";
+
+let heroes = [
+  { firstName: 'Tony', lastName: 'Stark', heroName: 'Iron Man' },
+  { firstName: 'Steve', lastName: 'Rogers', heroName: 'Captain America' },
+  { firstName: 'Barry', lastName: 'Allen', heroName: 'The Flash' },
+];
+
+///pure function
+const addInitialsToHeroes = heroes => heroes.map(hero => Object.assign({}, hero, {
+  initials: hero.firstName.charAt(0) + hero.lastName.charAt(0),
+}));
+
+let heroesWithInitials = addInitialsToHeroes(heroes);
+
+///impure function
+// function addInitialsToHeroes(heroes) {
+//   heroes.forEach(hero => {
+//     hero.initials = hero.firstName.charAt(0) + hero.lastName.charAt(0);
+//   });
+//
+//   return heroes;
+// }
+//
+// let heroesWithInitials = addInitialsToHeroes(heroes);
